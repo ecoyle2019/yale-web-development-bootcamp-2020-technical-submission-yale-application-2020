89518b208301d6529daf4e27740d5c97f0c64579
Ethan Coyle <ethan.coyle@yale.edu>
2020-04-09 20:04:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 name = "Ethan Coyle"
 age = 19
 favorite_food = "pizza"
-height_in_inches = 74
+height_in_inches = 67
 height_message = (height_in_inches < AVERAGE_HEIGHT_IN_INCHES) ? "You are not taller than average." : "You are taller than average."
 
 ## Do not modify below this line!
